a77cb044fbcd7a24065dc2e2eed7944d99583801
zhanglin <84386590@qq.com>
2018-04-22 11:08:47 +0800

    add annoations

---
+++ src/main/java/com/tecphp/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,16 @@
+package com.tecphp.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标识【线程安全】的类或写法
+ * Created by zhanglin on 2018/4/22.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
